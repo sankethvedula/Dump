ebb8d8d8c4f66291db00e5d4d3a378e5679d1d33
Sanketh Vedula <sankethvedula@users.noreply.github.com>
2015-12-29 15:56:59 +0530

    period extracted. predicting for the next bin.
    Taking the best prediction based on MAPE

--- customAlgo.R
+++ customAlgo.R
@@ -193,16 +193,202 @@ apply_HoltWinters <- function(detrendedTrainData)
     fcObject <- forecast(hwObject,h=100)	
 }
 
-compute_periodogram <- function(detrendedTrainData)
+find.freq_AR <- function(x)
 {
+  n <- length(x)
+  spec <- spec.ar(c(na.contiguous(x)),plot=FALSE)
+  if(max(spec$spec)>10) # Arbitrary threshold chosen by trial and error.
+  {
+    period <- round(1/spec$freq[which.max(spec$spec)])
+    if(period==Inf) # Find next local maximum
+    {
+      j <- which(diff(spec$spec)>0)
+      if(length(j)>0)
+      {
+        nextmax <- j[1] + which.max(spec$spec[j[1]:500])
+        if(nextmax <= length(spec$freq))
+          period <- round(1/spec$freq[nextmax])
+        else
+          period <- 1
+      }
+      else
+        period <- 1
+    }
+  }
+  else
+    period <- 1
+
+  return(period)
+}
+
+compute_periodogram <- function(detrendedTrainData,testData)
+{
+	testData <- testData
+	print(testData)
 	temp = spec.pgram(detrendedTrainData[,2],spans=c(19,19))
 	spectralFreq = temp$spec
 	timeFrequency = temp$freq
 	periodogramData <- cbind(spectralFreq,timeFrequency)
-	print(periodogramData)
+	#print(periodogramData)
+	bandwidth = temp$bandwidth
+	indexStep = as.integer(0.1/bandwidth)
+	if(indexStep > 20){
+	temp1 = periodogramData[1:indexStep,]
+	}else
+	{
+	temp1 = periodogramData
+	}
+	sortedTemp1 = temp1[order(temp1[,1],decreasing=TRUE),]
+	#sortedTemp1 <- colnames("Spectral Energy","Inverse Frequency")
+	top20periods <- 1/sortedTemp1[1:20,2]
+	print(top20periods)
+	predicted_bin <- predict_next_bin_detrended(top20periods,detrendedTrainData,testData)
+	#temp2 = periodogramData[indexStep:(2*indexStep),]
+	#sortedTemp2 = temp2[order(temp2[,1],decreasing=TRUE),]
+	#sortedTemp2 <- colnames("Spectral Energy","Inverse Frequency")
+	#top20periods <- 1/sortedTemp1[1:20,2]
+	#predict_next_bin_detrended(top20periods,detrendedTrainData)
+	return(predicted_bin)
 }
 
-data = read.csv("C:\\Users\\735201\\Desktop\\Sanketh-Test\\CPU_Util_data\\020.csv",sep=",")
+predict_next_bin_detrended <- function(top20periods,detrendedTrainData,testData)
+{
+	testData <- testData
+	index = 0
+	for(i in 1: length(top20periods))
+	{
+		current_period = top20periods[i]
+		temp = matrix(data = 0,nrow=current_period,ncol=1)
+
+		startIndex = 0
+		endIndex = current_period
+		repeat{
+		if(endIndex > length(detrendedTrainData[,2]))
+		{
+			temp1 = detrendedTrainData[startIndex:length(detrendedTrainData[,2]),2]
+			length1 = endIndex-length(detrendedTrainData[,2])
+			zero_temp = vector(,length1)
+			temp1 = c(temp1,zero_temp)
+			temp = cbind(temp,temp1)
+			break
+		}
+		#print(detrendedTrainData[startIndex:endIndex,2])
+		temp = cbind(temp,detrendedTrainData[startIndex:endIndex,2])
+		startIndex = startIndex + current_period
+		endIndex = startIndex + current_period
+		}
+	
+	
+    predicted_bin = vector(,current_period)
+	folds = length(detrendedTrainData[,2])/current_period
+	folds = as.integer(folds)
+	folds = folds + 2
+	for(k in 1: length(temp[,1]))
+	{
+	if(temp[k,folds]==0.000000)
+	{
+		pred_temp = sum(temp[k,])
+		pred_temp = pred_temp/(folds-1)
+		predicted_bin[k] = pred_temp
+	}
+	pred_temp = sum(temp[k,])
+	pred_temp = pred_temp/folds
+	predicted_bin[k] = pred_temp
+	}
+	
+	combined = c(detrendedTrainData[,2],predicted_bin)
+	predictedPlotsFolder = "C:\\Users\\735201\\Desktop\\Sanketh-Test\\CPU_Util_data\\Predicted_plots020\\"
+	lmFileName = paste(predictedPlotsFolder,"Prediction_with_period")
+	lmFileName = paste(lmFileName,top20periods[i])
+    lmFileName = paste(lmFileName,".jpg")
+	jpeg(lmFileName)
+	plot(combined,type="l",)
+	lines(combined[length(detrendedTrainData[,1]):(length(combined))],type="l",col="red")
+	#lines(combined[length(detrendedTrainData[,1]):length(combined)],type="l",col="red")
+	dev.off()
+	testData <- testData
+	calculate_errors(predicted_bin,testData)
+	}
+	return(predicted_bin)
+}
+
+calculate_errors <- function(predicted_bin,testData)
+{
+	testData <- as.data.frame(testData)
+	error1 = testData[1:length(predicted_bin),2] - predicted_bin 
+	MAPE =0
+	for(k in 1:length(error1))
+  {
+  if(testData[k,2] != 0)
+  {
+  MAPE = MAPE + (abs(error1[k])/testData[k,2])   
+  }
+  else 
+  {
+  MAPE = MAPE + abs(error1[k])
+  }
+  }
+  MAPE = MAPE/length(error1)
+
+  
+  print(MAPE)
+}
+
+predict_next_bin_AR <- function(detrendedTrainData, period)
+{
+	current_period = period
+		temp = matrix(data = 0,nrow=current_period,ncol=1)
+
+		startIndex = 0
+		endIndex = current_period
+		repeat{
+		if(endIndex > length(detrendedTrainData[,2]))
+		{
+			temp1 = detrendedTrainData[startIndex:length(detrendedTrainData[,2]),2]
+			length1 = endIndex-length(detrendedTrainData[,2])
+			zero_temp = vector(,length1)
+			temp1 = c(temp1,zero_temp)
+			temp = cbind(temp,temp1)
+			break
+		}
+		#print(detrendedTrainData[startIndex:endIndex,2])
+		temp = cbind(temp,detrendedTrainData[startIndex:endIndex,2])
+		startIndex = startIndex + current_period
+		endIndex = startIndex + current_period
+		}
+	
+	
+    predicted_bin = vector(,current_period)
+	folds = length(detrendedTrainData[,2])/current_period
+	folds = as.integer(folds)
+	folds = folds + 2
+	for(k in 1: length(temp[,1]))
+	{
+	if(temp[k,folds]==0.000000)
+	{
+		pred_temp = sum(temp[k,])
+		pred_temp = pred_temp/(folds-1)
+		predicted_bin[k] = pred_temp
+	}
+	pred_temp = sum(temp[k,])
+	pred_temp = pred_temp/folds
+	predicted_bin[k] = pred_temp
+	}
+	
+	combined = c(detrendedTrainData[,2],predicted_bin)
+	predictedPlotsFolder = "C:\\Users\\735201\\Desktop\\Sanketh-Test\\CPU_Util_data\\Predicted_plots020\\"
+	lmFileName = paste(predictedPlotsFolder,"Prediction_with_period_AR")
+	lmFileName = paste(lmFileName,top20periods[i])
+    lmFileName = paste(lmFileName,".jpg")
+	jpeg(lmFileName)
+	plot(combined,type="l",)
+	lines(combined[length(detrendedTrainData[,1]):(length(combined))],type="l",col="red")
+	#lines(combined[length(detrendedTrainData[,1]):length(combined)],type="l",col="red")
+	dev.off()
+
+}
+
+data = read.csv("C:\\Users\\735201\\Desktop\\Sanketh-Test\\CPU_Util_data\\1.csv",sep=",")
 relevantData = data[,2:3]
 temp = relevantData[,1]
 temp = strptime(temp,format="%m/%d/%Y %H:%M")
@@ -218,11 +404,17 @@ vari <- test(trainData)
 predict_lm(trainData,testData,vari[2])
 detrendedTrainData <- detrend(coeff,trainData)
 
-compute_periodogram(detrendedTrainData)
+predicted_bin <- compute_periodogram(detrendedTrainData,testData)
+
+
+
+period <- find.freq_AR(detrendedTrainData[,2])
+predict_next_bin_AR(detrendedTrainData,period)
+#calculate_peaks(periodogramData)
 
 apply_AutoArima(detrendedTrainData)
 decompose_data(detrendedTrainData)
 apply_HoltWinters(detrendedTrainData)
 
 
-print(relevantData)
+#print(relevantData)
